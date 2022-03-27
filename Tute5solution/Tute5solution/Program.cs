0f02c4eac3dcbe6d62c1c04408974a88a7a0e845
fdelanty <fdelanty@ICT-DWR7BS2>
2022-03-28 10:23:53 +1100

    employee class added

--- Tute5solution/Tute5solution/Program.cs
+++ Tute5solution/Tute5solution/Program.cs
@@ -8,10 +8,60 @@ namespace Tute5solution
 {
     class Program
     {
+        enum Gender
+        {
+            Male,
+            Female,
+            Other
+        }
         static void Main(string[] args)
         {
-            //comment
-            //comment changes from fin
+            string input = "";
+            //printList(generateEmployees());
+            while (input != "exit")
+            {
+                Console.WriteLine("Search for which gender? ");
+                input = Console.ReadLine();
+                printList(filterByGender(generateEmployees(), input));
+            }
         }
+
+        static List<Employee> generateEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee a = new Employee { name = "Alfred", id = 101, gender = "M" };
+            Employee b = new Employee { name = "Bruce", id = 102, gender = "M" };
+            Employee e = new Employee { name = "Shelley", id = 100, gender = "F" };
+
+            employees.Add(a);
+            employees.Add(b);
+            employees.Add(e);
+
+            return employees;
+        }
+
+        static void printList(List<Employee> list)
+        {
+            foreach (Employee e in list)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static List<Employee> filterByGender(List<Employee> staff, string gender)
+        {
+            List<Employee> filteredList = new List<Employee>(); ;
+
+            foreach (Employee e in staff)
+            {
+                if (e.gender == gender)
+                {
+                    filteredList.Add(e);
+                }
+            }
+
+            return filteredList;
+        }
+
     }
 }
